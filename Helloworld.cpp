8543f7289315a679e3a71681e6d59475085a5f28
Austin <zidong1992@gmail.com>
2016-05-22 10:02:57 +0800

    Boring

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include <vector>
-#include<string>
-#include<iostream>
+#include <string>
 #include <fstream>
-#include<curses.h>
-#include<ncurses.h>
-using namespace std;
+#include <curses.h>
+#include <pthread.h>
+#include <ncurses.h>
+#include <boost/algorithm/string.hpp>
 
+using namespace std;
+#define NUM_THREADS 5
+
+void * sayHi(void* args){
+    cout << "1" << endl;
+    cout << "2" << endl;
+    cout << "3" << endl;
+    cout << "4" << endl;
+};
+bool checkAccount(string un, string pw){
+    ifstream checker;
+    checker.open("File");
+    for (string s; s!="#EOF";) {
+      //   getline(checker, s);
+      checker >> s;
+      if(s == "#EOF") break;
+      if(s == "Buyer" || s == "Member" || s == "Seller") continue;
+      if (s == un){
+          checker >> s;
+          if (s == pw) return true;
+      }
+      getline(checker, s);
+    }
+    checker.close();
+    return false;
+};
 bool accept(){
     std::cout << "Do you want to procced (y or n)? \n" << std::endl;
     char answer = 0;
@@ -14,12 +40,10 @@ bool accept(){
     if(answer == 'y')return true;
     return false;
 }
-
 struct Books{
     string name;
     int number;
 };
-
 // std::vector <Entry> phone_book[100];
 // std::vector<char> v;
 class Book {
@@ -42,9 +66,7 @@ class Book {
         }
 
 };
-
-class Line
-{
+class Line{
    public:
       void setLength( double len );
       double getLength( void );
@@ -53,25 +75,32 @@ class Line
    private:
       double length;
 };
-
-Line::Line(void)
-{
+Line::Line(void){
     cout << "Object is being created" << endl;
 }
-
-void Line::setLength( double len )
-{
+void Line::setLength( double len ){
     length = len;
 }
-
-double Line::getLength( void )
-{
+double Line::getLength( void ){
     return length;
 }
-
 int main( )
 {
-   Line line;
+
+
+   /*
+   pthread_t tids[NUM_THREADS];
+    for(int i = 0; i < NUM_THREADS; ++i)
+    {
+        //参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
+        int ret = pthread_create(&tids[i], NULL, sayHi, NULL);
+        if (ret != 0)
+        {
+           cout << "pthread_create error: error_code=" << ret << endl;
+        }
+    }
+    //等各个线程退出后，进程才结束，否则进程强制结束了，线程可能还没反应过来；
+    pthread_exit(NULL);
 
    // line.setLength(6.0);
    // cout << "Length of line : " << line.getLength() <<endl;
@@ -80,30 +109,47 @@ int main( )
    std::cout << "KALAB" << std::endl;
 
    return 0;
-}
-/*
-int main(){
-    cout << "test\n";
-    int acb = 123;
-    string str1 = "Hi";
-    string str2 = "You";
-    string str3;
-    str3 = str1 + str2;
-    std::cout << str3 << std::endl;
-    Book book1;
-    book1.length = 10;
-    book1.breadth = 20;
-    book1.height = 40;
-    cout << book1.getVolume() <<std::endl;
-    fstream file;
-    file.open("File",ios::in|ios::out);
-    file << "asdflkajsdf;o";
-    initscr();
-    box(stdscr, ACS_VLINE, ACS_HLINE);
-    move(LINES/2, COLS/2);
-    waddstr(stdscr, "Hello, world!");
-    refresh();
+   Line line;
+   cout << "test\n";
+   int acb = 123;
+   string str1 = "Hi";
+   string str2 = "You";
+   string str3;
+   str3 = str1 + str2;
+   std::cout << str3 << std::endl;
+   Book book1;
+   book1.length = 10;
+   book1.breadth = 20;
+   book1.height = 40;
+   cout << book1.getVolume() <<std::endl;
+   fstream file;
+   file.open("File",ios::in|ios::out);
+   file << "asdflkajsdf;o";
+   initscr();
+   box(stdscr, ACS_VLINE, ACS_HLINE);
+   move(LINES/2, COLS/2);
+   waddstr(stdscr, "Hello, world!");
+   refresh();
 
-    return 0;
+   return 0;
+
+   */
+   // ifstream checker;
+   // checker.open("File");
+   // string s;
+   if(checkAccount("austin","test"))cout << "abc" << endl;
+   else cout << "error" << endl;
+   /*
+   int i = 20;
+   while (i>0) {
+    //    getline(checker,s);
+       if(s == "#EOF")break;
+    //    cout << s << endl;
+       checker >> s;
+       cout << s << endl;
+    //    getline(checker,s);
+    //    cout << s << endl;
+       i--;
+   }
+   */
 }
-*/
--- WDE-Busniess/Server.cpp
+++ WDE-Busniess/Server.cpp
@@ -3,105 +3,143 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <pthread.h>
+using namespace std;
+
 void fail() { std::cout << "Error!" << std::endl; }
+
 class User {
 protected:
   int id;
   double balance;
-public:
   std::string userName;
   std::string password;
-
-  bool onlinestatus;
-
-  bool checkAccount(int number) {
-
+public:
+  bool onlineStatus;
+  bool checkAccount(string un, string pw){
+      ifstream checker;
+      checker.open("UserData.dat");
+      for (string s; s!="#EOF";) {
+        //   getline(checker, s);
+        checker >> s;
+        if(s == "#EOF") break;
+        if(s == "Buyer" || s == "Member" || s == "Seller") continue;
+        if (s == un){
+            checker >> s;
+            if (s == pw) return true;
+        }
+        getline(checker, s);
+      }
+      checker.close();
+      return false;
   };
-
-  // 查看用户个人信息
-  void viewaccount() {}
-  void viewitems() {}
-  void signUp(){
-
-  }
+  virtual void viewAccount() = 0;
+  virtual void signUp() = 0;
+  virtual void logout() = 0;
   void login() {
     std::string userName;
-    std::cout << "Username: " << std::endl;
+    std::cout << "Username: ";
     std::cin >> userName;
     std::string password;
-    std::cout << "Password: " << std::endl;
+    std::cout << "Password: ";
     std::cin >> password;
-};
-  void logout() { std::cout << "Logout Success!" << std::endl; }
+    if(this-> checkAccount())cout << "Success!" << endl;
+    else cout << "try again" << endl;
+  };
 };
 
 class Buyer : public User {
+protected:
+    int;
+public:
   void recharge(double money) { balance += money; }
-  bool Buy(int item_id, int item_counts, int item_price){
+  bool Buy(int item_id, int item_counts, int item_price){};
+  void signUp(){
+      std::string userName;
+      std::cout << "Username: ";
+      std::cin >> userName;
+      checkAccount();
+      if
+      std::string password;
+      std::cout << "Password: ";
+      std::cin >> password;
+  }
+};
 
-        }
-    };
-  class Member : public Buyer {
+class Member : public Buyer {
+private:
     int level;
     int token;
-    //代币与现金1:1兑换
-    void exchange(int exchangeNumber) {
-      if (exchangeNumber < token) {
-        this->balance += exchangeNumber;
-        token -= exchangeNumber;
-      }
-      else
-        fail();
+public:
+
+  //代币与现金1:1兑换
+  void exchange(int exchangeNumber) {
+    if (exchangeNumber < token) {
+      this->balance += exchangeNumber;
+      token -= exchangeNumber;
     }
-    };
-  class Seller : public User {
-    map ownItems;
-    void addItems;
-    void checkItems;
-  };
-
-  class Sales_item {
-  public:
-    int id;
-    std::string name;
-    std::string owner;
-    int amount;
-    double price;
-
-    int productionDate;
-    int shelfLife;
-    std::string discountReason;
-    int discountPeriod;
-  };
-
-  class Food : public Sales_item {
-    std::string taste;
-  };
-
-  class Electronics : public Sales_item {
-    std::string type;
-  };
-
-  class Daily : public Sales_item {
-    std::string Brand;
-  };
-  class System {
-      saveGame(){
-          s;
-      };
-      loadGame(){
-
-      };
-  };
-  // class admin {};
-  int main() {
-    //读文件，读取用户格式
-
-
-    while (true) {
-      // cin 登录
-
-      // cin 决定操作
-    }
-    return 0;
+    else
+      fail();
   }
+};
+
+class Seller : public User {
+  map ownItems;
+  void addItems;
+  void checkItems;
+};
+
+////////////////////////////////////////
+
+class Sales_item {
+public:
+  int id;
+  std::string name;
+  std::string owner;
+  int amount;
+  double price;
+
+  int productionDate;
+  int shelfLife;
+  std::string discountReason;
+  int discountPeriod;
+};
+
+class Food : public Sales_item {
+  std::string taste;
+};
+
+class Electronics : public Sales_item {
+  std::string type;
+};
+
+class DailyUse : public Sales_item {
+  std::string Brand;
+};
+
+////////////////////////////////////////
+
+class System {
+    map<key, value> userinfo;
+    map<key, value> iteminfo;
+    saveGame(){
+        //save userinfo
+        //save iteminfo
+    };
+    loadGame(){
+
+    };
+};
+////////////////////////////////////////
+
+int main() {
+  //读文件，读取用户格式
+
+
+  while (true) {
+    // cin 登录
+
+    // cin 决定操作
+  }
+  return 0;
+}
